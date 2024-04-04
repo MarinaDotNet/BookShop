8d3e5ec733181a6212a48eb0f302f273b6f8197a
Marina Sichova <msichova@outlook.com>
2024-04-04 16:01:02 +0800

    Created ProductDatabaseSettings.cs
    Class to store appsettings.jsoon file's property value for ConnectionMongoStock

---
+++ BookShop.API/Models/ProductDatabaseSettings.cs
@@ -0,0 +1,12 @@
+﻿/*
+ * Class to store appsettings.json file's ConnectionMongoStock property values
+ */
+namespace BookShop.API.Models
+{
+    public class ProductDatabaseSettings
+    {
+        public string Connection { get; set; } = string.Empty!;
+        public string DatabaseName { get; set;} = string.Empty!;
+        public string CollectionName { get; set; } = string.Empty!;
+    }
+}
