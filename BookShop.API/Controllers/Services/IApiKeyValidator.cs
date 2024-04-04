24bb71775a9189c9584bd67bfe3ee45adbc82e0b
Marina Sichova <msichova@outlook.com>
2024-04-04 20:43:37 +0800

    Created IApiKeyValidator.cs
    interface to validate api key

---
+++ BookShop.API/Controllers/Services/IApiKeyValidator.cs
@@ -0,0 +1,10 @@
+﻿/*
+ * Interface to validate Api Key
+ */
+namespace BookShop.API.Controllers.Services
+{
+    public interface IApiKeyValidator
+    {
+        bool isValid(string apiKey);
+    }
+}
