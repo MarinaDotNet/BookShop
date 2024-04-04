0b762a9fc5f8ecafcb4491371b0f7b3976eb284b
Marina Sichova <msichova@outlook.com>
2024-04-04 16:17:12 +0800

    Crated StockController.cs
    API controller

---
+++ BookShop.API/Controllers/StockController.cs
@@ -0,0 +1,71 @@
+﻿using BookShop.API.Controllers.Services;
+using BookShop.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+
+namespace BookShop.API.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    [Consumes("application/json")]
+    public class StockV1Controller(StockDBServices services) : ControllerBase
+    {
+        private readonly StockDBServices _services = services;
+
+        #region of HttpGet Methods
+        #region of simple HttpGet Methods
+        /*!ATTENTION! may be overflow error, or slowdown preformance.Depends on current size of database
+         * returns all data from database collection
+        */
+        [HttpGet, Route("books/all")]
+        public async Task<ActionResult<List<Product>>> GetAllProducts()
+        {
+            try
+            {
+                var products = await _services.GetAllBooksAsync();
+
+                return products is null || products.Count == 0 ?
+                    NotFound() : Ok(products);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+
+
+        #endregion
+        #endregion
+        #region of HttpMethods for manipulations with Collection
+        [HttpPost, Route("book/add")]
+        public async Task<ActionResult> PostProduct([FromQuery]Product product)
+        {
+            try
+            {
+                if(product.Genres is null || product.Genres.Length == 0)
+                {
+                    product.Genres = ["unspecified"];
+                }
+                Product book = new()
+                {
+                    Title = product.Title,
+                    Author = product.Author,
+                    Annotation = product.Annotation,
+                    IsAvailable = product.IsAvailable,
+                    Language = product.Language,
+                    Link = !string.IsNullOrEmpty(product.Link.ToString()) && Uri.IsWellFormedUriString(product.Link.ToString(), UriKind.Absolute) ?
+                    product.Link : new Uri("about:blank"),
+                    Genres = [.. product.Genres],
+                    Price = product.Price > 0 ? product.Price : 0
+                };
+                await _services.AddNewAsync(book);
+                return Ok("Successfully added: " + book.ToJson());
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+        #endregion
+    }
+}
