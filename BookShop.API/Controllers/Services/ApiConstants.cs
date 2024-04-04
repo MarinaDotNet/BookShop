05ba00f398afca9bdddfddd22eae271cf1d0a1d4
Marina Sichova <msichova@outlook.com>
2024-04-04 19:11:51 +0800

    Created ApiConstants.cs

---
+++ BookShop.API/Controllers/Services/ApiConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace BookShop.API.Controllers.Services
+{
+    public class ApiConstants
+    {
+        public const string ApiKeyHeader = "x-API-key";
+        public const string ApiKeyName = "ApiKey";
+        public const string ApiVersionHeader = "ApiVersion-BooksStore";
+    }
+}
