498a63dd4d4df4cd8c9f3ea8fdfdbe6647a09668
Marina Sichova <msichova@outlook.com>
2024-04-05 17:36:52 +0800

    Created FilterProducts.cs
    Model for filtering products by specified query

---
+++ BookShop.API/Models/FilterProducts.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Model to Filter Products by specified query
+ * 
+ */
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.API.Models
+{
+    public class FilterProducts
+    {
+        public string? Annotation { get; set; }
+
+        public string? Author { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public string? Title { get; set; }
+
+        public string[] ? Genres { get; set; }
+
+        public string? Language { get; set; }
+
+
+        [Range(0, int.MaxValue)]
+        public decimal MinPrice { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public decimal MaxPrice { get; set; }
+    }
+}
