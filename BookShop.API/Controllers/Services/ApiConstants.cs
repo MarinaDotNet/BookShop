41f357a6b8ef12136557e8d3d03b85a6a6043a3b
Marina Sichova <msichova@outlook.com>
2024-04-05 13:47:24 +0800

    Updated ApiConstants.cs
    Added constants User and Admin for authentication roles

--- BookShop.API/Controllers/Services/ApiConstants.cs
+++ BookShop.API/Controllers/Services/ApiConstants.cs
@@ -5,5 +5,7 @@
         public const string ApiKeyHeader = "x-API-key";
         public const string ApiKeyName = "ApiKey";
         public const string ApiVersionHeader = "ApiVersion-BooksStore";
+        public const string User = "user";
+        public const string Admin = "admin";
     }
 }
