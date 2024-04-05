f177dfaec6f5cc9b62b4768c7133f1f80e935402
Marina Sichova <msichova@outlook.com>
2024-04-05 17:22:06 +0800

    Created LoginModel.cs and RegistrationModel.cs

---
+++ BookShop.API/Models/Authentication/LoginModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.API.Models.Authentication
+{
+    public class LoginModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please, enter your login: user name or email")]
+        [DisplayName("Loging")]
+        public string UserLogin { get; set; } = string.Empty!;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please, enter your password")]
+        [PasswordPropertyText(true)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty!;
+    }
+}
---
+++ BookShop.API/Models/Authentication/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.API.Models.Authentication
+{
+    public class RegisterModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Unique Login is required")]
+        public string Login { get; set; } = string.Empty!;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Unique Email is required")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string EmailAddress { get; set; } = string.Empty!;
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please enter your Password, Password is required")]
+        [DataType(DataType.Password)]
+        [PasswordPropertyText]
+        public string Password { get; set; } = string.Empty!;
+    }
+}
