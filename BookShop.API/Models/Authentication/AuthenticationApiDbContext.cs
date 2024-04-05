31065f0ed13f6480724f9864d3e496a67a8c32ca
Marina Sichova <msichova@outlook.com>
2024-04-05 17:21:40 +0800

    Created AuthenticationApiDbContext.cs
    Extends IdentityDbContext

---
+++ BookShop.API/Models/Authentication/AuthenticationApiDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookShop.API.Models.Authentication
+{
+    public class AuthenticationApiDbContext(DbContextOptions<AuthenticationApiDbContext> options) : IdentityDbContext<ApiUser>(options)
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
