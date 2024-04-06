8f78f779105792778eae764f1bb7b5e354b256e9
Marina Sichova <msichova@outlook.com>
2024-04-06 16:01:37 +0800

    Created PageModel.cs
    Model for user requests

---
+++ BookShop.API/Models/PageModel.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * From user request
+ */
+using System.ComponentModel;
+
+namespace BookShop.API.Models
+{
+    public class PageModel
+    {
+        public int RequestedPage { get; set; } = Query.MinPossiblePage;
+        public int QuantityPerPage { get; set; } = Query.MinPossibleQuantityAtPage;
+        public bool InAscendingOrder { get; set; } = false;
+
+        [DefaultValue("Price")]
+        public string OrderBy { get; set; } = "Price";
+    }
+}
