06ec20f27d3ae37a49d5ba6760ab821404fc4d76
Marina Sichova <msichova@outlook.com>
2024-04-15 13:10:06 +0800

    Update AuthenticationApiDbContext.cs
    To ensure that in local database has records of default user and admin accounts

--- BookShop.API/Models/Authentication/AuthenticationApiDbContext.cs
+++ BookShop.API/Models/Authentication/AuthenticationApiDbContext.cs
@@ -1,12 +1,69 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BookShop.API.Models.Authentication
 {
-    public class AuthenticationApiDbContext(DbContextOptions<AuthenticationApiDbContext> options) : IdentityDbContext<ApiUser>(options)
+    public class AuthenticationApiDbContext(DbContextOptions<AuthenticationApiDbContext> options) : IdentityDbContext<ApiUser, IdentityRole, string>(options)
     {
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            string adminId = Guid.NewGuid().ToString();
+            string userId = Guid.NewGuid().ToString();
+            string roleAdminId = Guid.NewGuid().ToString();
+            string roleUserId = Guid.NewGuid().ToString();
+
+            var hasher = new PasswordHasher<ApiUser>();
+            builder.Entity<ApiUser>().HasData(
+                new ApiUser
+                {
+                    Id = adminId,
+                    Email = "admin@email.com",
+                    NormalizedEmail = "admin@email.com",
+                    UserName = "admin",
+                    NormalizedUserName = "admin",
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    PasswordHash = hasher.HashPassword(null!, "P@ssw0rd")
+                },
+                new ApiUser
+                {
+                    Id = userId,
+                    Email = "user@email.com",
+                    NormalizedEmail = "user@email.com",
+                    UserName = "user",
+                    NormalizedUserName = "user",
+                    EmailConfirmed = true,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    PasswordHash = hasher.HashPassword(null!, "P@ssw0rd")
+                });
+
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole
+                {
+                    Id = roleAdminId,
+                    Name = "admin",
+                    NormalizedName = "admin"
+                },
+                new IdentityRole
+                {
+                    Id = roleUserId,
+                    Name = "user",
+                    NormalizedName = "user"
+                });
+
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = roleAdminId,
+                    UserId = adminId
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = roleUserId,
+                    UserId = userId
+                });
+
             base.OnModelCreating(builder);
         }
     }
