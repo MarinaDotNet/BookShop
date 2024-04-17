99451691444549276d59873a75515ebe88541c27
Marina Sichova <msichova@outlook.com>
2024-04-17 15:41:35 +0800

    Created DeleteAccountModel.cs
    DeleteAccountModel.cs extends DeleteModel.cs

---
+++ BookShop.API/Models/Authentication/DeleteAccountModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.API.Models.Authentication
+{
+    public class DeleteAccountModel : DeleteModel
+    {
+        [Required(ErrorMessage = "Please enter account email to delete account")]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Account Email")]
+        public string AccountEmail { get; set; } = string.Empty!;
+
+        [Required(ErrorMessage = "Please enter confirm account email to delete account")]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        [Compare("AccountEmail", ErrorMessage = "Account Email and Confirmation Email do not match.")]
+        [Display(Name = "Confirm Email")]
+        public string AccountEmailConfirm { get; set; } = string.Empty!;
+    }
+}
