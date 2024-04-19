94f8b5b0153e83863449f3da87376511ffe7541a
Marina Sichova <msichova@outlook.com>
2024-04-19 12:59:18 +0800

    Created Order.cs
    Added Entity Model class for the SQL table Orders

---
+++ BookShop.API/Models/Order.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Entity Model class for the SQL table Orders
+ */
+using BookShop.API.Models.Authentication;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookShop.API.Models
+{
+    [Table("Orders")]
+    public class Order
+    {
+        [ForeignKey("UserId")]
+        public string UserId { get; set; } = string.Empty!;
+
+        public string OrderId { get; set; } = string.Empty!;
+
+        public ApiUser User { get; set; } = new();
+        public List<string>? ProductsId { get; set; } = new();
+
+        [Precision(18, 2)]
+        public decimal TotalPrice { get; set; }
+
+        public DateTime OrderDateTime { get; set; }
+
+        public bool SubmittedOrder { get; set; }
+    }
+}
