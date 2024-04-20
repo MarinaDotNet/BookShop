3f36e11998f372f4c6437707acdfd667ddb46d11
Marina Sichova <msichova@outlook.com>
2024-04-20 15:49:09 +0800

    Created OrderDisplayModel.cs
    Added new class Model to return some Order object details

---
+++ BookShop.API/Models/OrderDisplayModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BookShop.API.Models
+{
+    public class OrderDisplayModel(Order order, string additionalInformation)
+    {
+        public string OrderId { get; set; } = order.OrderId;
+        public List<string> ProductsId { get; set; } = [..order.ProductsId];
+        public decimal TotalPrice { get; set; } = order.TotalPrice;
+        public DateTime OrderDateTime { get; set; } = order.OrderDateTime;
+        public bool IsSubmitted { get; set; } = order.SubmittedOrder;
+        public string Notes { get; set; } = additionalInformation;
+    }
+}
