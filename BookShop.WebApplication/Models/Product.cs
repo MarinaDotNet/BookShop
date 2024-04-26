4c89c91738eedc2387a647dfb425519d68959ca1
Marina Sichova <msichova@outlook.com>
2024-04-26 21:19:52 +0800

    Created Product.cs
    Entity model for received result from API

---
+++ BookShop.WebApplication/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+/*
+ * Entity Model class for storing result from API
+ * 
+ */
+namespace BookShop.WebApplication.Models
+{
+    public class Product
+    {
+        public string? Id { get; set; }
+
+        public string? Annotation { get; set; }
+
+        public string? Author { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public string? Title { get; set; }
+
+        public string[] Genres { get; set; } = ["undefined"];
+
+        public string? Language { get; set; }
+
+        public Uri Link { get; set; } = new Uri("about:blank");
+
+        [Range(0, int.MaxValue)]
+        [Precision(18, 2)]
+        public decimal Price { get; set; } = 0;
+    }
+}
