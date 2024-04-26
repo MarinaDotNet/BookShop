4cc65b97dac5d65fff021390b04650901bacd158
Marina Sichova <msichova@outlook.com>
2024-04-26 18:27:16 +0800

    Created ApplicationConstants.cs

---
+++ BookShop.WebApplication/Services/ApplicationConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop.WebApplication.Services
+{
+    public class ApplicationConstants
+    {
+        public const string ApiKeyHeader = "x-API-key";
+        public const string ApiKeyName = "ApiKey";
+        public const string ApiVersionHeader = "ApiVersion-BooksStore";
+        public const string ApiVersionValue = "2";
+        public const string TokenClaimRole = "user";
+    }
+}
