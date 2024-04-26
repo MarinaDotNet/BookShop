3ffe58a9edef589c14d10e77c71fb834c3557768
Marina Sichova <msichova@outlook.com>
2024-04-26 20:26:12 +0800

    Updated HomeController.cs
    Added necessary headers to HttpClient, to sending requests to the API

--- BookShop.WebApplication/Controllers/HomeController.cs
+++ BookShop.WebApplication/Controllers/HomeController.cs
@@ -1,16 +1,39 @@
 using BookShop.WebApplication.Models;
+using BookShop.WebApplication.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 
 namespace BookShop.WebApplication.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private IMemoryCache _tokenCache;
+        private readonly HttpClient _httpClient = new();
+        private readonly IConfiguration _configuration;
+        public HomeController(ILogger<HomeController> logger, 
+            IMemoryCache tokenCache,
+            IConfiguration configuration)
         {
             _logger = logger;
+            _tokenCache = tokenCache;
+            _configuration = configuration;
+
+            _httpClient.DefaultRequestHeaders.Add(ApplicationConstants.ApiKeyHeader,
+                _configuration[ApplicationConstants.ApiKeyName]);
+
+            if(_tokenCache.TryGetValue("token", out TokenGenerator? token))
+            {
+                _httpClient.DefaultRequestHeaders.Add(ApplicationConstants.ApiVersionHeader, ApplicationConstants.ApiVersionValue);
+                _httpClient.DefaultRequestHeaders.Authorization = 
+                        new AuthenticationHeaderValue("Bearer", token!.Value);
+            }
+            else
+            {
+                _httpClient.DefaultRequestHeaders.Add(ApplicationConstants.ApiVersionHeader, "3");
+            }
         }
 
         public IActionResult Index()
