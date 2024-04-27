47702c6648986c6808fcc6b3f860e207c4411301
Marina Sichova <msichova@outlook.com>
2024-04-27 14:18:54 +0800

    Created UrlRoute.cs
    To store all url's for the requests to the API

---
+++ BookShop.WebApplication/Models/UrlRoute.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * To store all url's for the requests to the API 
+ **/
+namespace BookShop.WebApplication.Models
+{
+    /**
+    * Url's requests to the API StockController
+    * */
+    public class UrlStockRoute
+    {
+        //returns first 5 available  in stock products with highest price,
+        //has access for not signed in users
+        //for index page
+        public Uri GetFiveMostExpensiveProducts { get; } = new Uri("https://localhost:7257/books/filter?OrderBy=Price");
+    }
+
+    /**
+    * Url's requests to the API OrderController
+    * */
+    public class UrlOrderRoute
+
+    {
+
+    }
+}
