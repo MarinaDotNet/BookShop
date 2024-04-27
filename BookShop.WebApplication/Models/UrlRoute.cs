cf871e4728785f875e03a7e3fdc506288e2057be
Marina Sichova <msichova@outlook.com>
2024-04-27 17:17:13 +0800

    Updated UrlRoute.cs
    Added parameter public Uri GetProductById { get; }
    link returns product by requested id, if id is valid and product  is available in stock, requers valid product.Id in query

--- BookShop.WebApplication/Models/UrlRoute.cs
+++ BookShop.WebApplication/Models/UrlRoute.cs
@@ -12,6 +12,11 @@ namespace BookShop.WebApplication.Models
         //has access for not signed in users
         //for index page
         public Uri GetFiveMostExpensiveProducts { get; } = new Uri("https://localhost:7257/books/filter?OrderBy=Price");
+
+        //returns product by requested id, if id is valid and product  is available in stock
+        //has access for not signed in users
+        //must add valid id into query
+        public Uri GetProductById { get; } = new Uri("https://localhost:7257/book/id");
     }
 
     /**
