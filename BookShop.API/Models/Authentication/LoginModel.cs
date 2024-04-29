dbb94b8305a0d7464b1f01e355ebdc280608141c
Marina Sichova <msichova@outlook.com>
2024-04-29 13:51:14 +0800

    Updated LoginModel.cs
    Changed parameter from Login into Email

--- BookShop.API/Models/Authentication/LoginModel.cs
+++ BookShop.API/Models/Authentication/LoginModel.cs
@@ -5,9 +5,10 @@ namespace BookShop.API.Models.Authentication
 {
     public class LoginModel
     {
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Please, enter your login: user name or email")]
-        [DisplayName("Loging")]
-        public string UserLogin { get; set; } = string.Empty!;
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please, enter your email")]
+        [DisplayName("Email")]
+        [EmailAddress(ErrorMessage = "Please, enter valid email address")]
+        public string UserEmail { get; set; } = string.Empty!;
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Please, enter your password")]
         [PasswordPropertyText(true)]
