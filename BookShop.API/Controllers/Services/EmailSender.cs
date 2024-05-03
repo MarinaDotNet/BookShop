cb478b7949f1b9d9a4454ebec6bc2a2d4d87ba9a
Marina Sichova <msichova@outlook.com>
2024-05-03 16:40:24 +0800

    Created EmailSender.cs

---
+++ BookShop.API/Controllers/Services/EmailSender.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.IdentityModel.Tokens;
+using System.Net;
+using System.Net.Mail;
+
+namespace BookShop.API.Controllers.Services
+{
+    public class EmailSender
+    {
+        public void SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            MailMessage mailMessage = new();
+            SmtpClient smtpClient = new();
+
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential("msichova.net@gmail.com", "O424o39989");
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.Host = "smtp.gmail.com";
+            smtpClient.EnableSsl = true;
+            smtpClient.Port = 587;
+
+            mailMessage.From = new MailAddress("msichova.net@gmail.com");
+            mailMessage.To.Add(new MailAddress(email));
+            mailMessage.Subject = subject;
+            mailMessage.IsBodyHtml = true;
+            mailMessage.Body = htmlMessage;
+
+            
+            smtpClient.Send(mailMessage);
+        }
+    }
+}
