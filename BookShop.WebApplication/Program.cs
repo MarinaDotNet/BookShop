015c3d86d73362ce6d7794a4ab3b2e09200a723c
Marina Sichova <msichova@outlook.com>
2024-05-03 12:18:30 +0800

    Updated BookShop.WebApplication
    Added user secrets, sensitive information removed from appsettings.json file.

--- BookShop.WebApplication/Program.cs
+++ BookShop.WebApplication/Program.cs
@@ -4,6 +4,8 @@ using BookShop.WebApplication.Data;
 using BookShop.WebApplication.Models;
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddUserSecrets<StartupBase>();
+
 builder.Services.AddDbContext<BookShopWebApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionToSQL") ?? throw new InvalidOperationException("Connection string 'BookShopWebApplicationContextConnection' not found.")));
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<BookShopWebApplicationContext>();
