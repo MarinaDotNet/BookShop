90f292e522621deb5aed4f13af10dd0555291b79
Marina Sichova <msichova@outlook.com>
2024-05-04 15:35:51 +0800

    Added
    EmailSender.cs and ApiConstants.cs from BookShop.API project

---
+++ BookShop.WebApplication/Services/ApiConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace BookShop.API.Controllers.Services
+{
+    public class ApiConstants
+    {
+        public const string ApiKeyHeader = "x-API-key";
+        public const string ApiKeyName = "ApiKey";
+        public const string ApiVersionHeader = "ApiVersion-BooksStore";
+        public const string User = "user";
+        public const string Admin = "admin";
+        public const string CorsNameUser = "MyPolicyForUser";
+        public const string CorsNameAdmin = "MyPolicyForAdmin";
+        public const string SendGridApiKeyName = "SendGridApiKey";
+        public const string SendEmailName = "SendEmail";
+    }
+}
