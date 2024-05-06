865d30d3bce3e1815cdab74470aeb0729424ece4
Marina Sichova <msichova@outlook.com>
2024-05-06 13:40:26 +0800

    Updated UrlRoute.cs

--- BookShop.WebApplication/Models/UrlRoute.cs
+++ BookShop.WebApplication/Models/UrlRoute.cs
@@ -1,4 +1,7 @@
-﻿/**
+﻿
+using Microsoft.Identity.Client;
+
+/**
  * To store all url's for the requests to the API 
  **/
 namespace BookShop.WebApplication.Models
@@ -17,6 +20,19 @@ namespace BookShop.WebApplication.Models
         //has access for not signed in users
         //must add valid id into query
         public Uri GetProductById { get; } = new Uri("https://localhost:7257/book/id");
+
+        //return quantity of all products in database with default filters
+        public Uri GetQuantityAll { get; } = new Uri("https://localhost:7257/books/count/all");
+
+        //returns list of products
+        public Uri GetAllProductsStandard(int page, int quantity, bool isAscending)
+        {
+            return new Uri("https://localhost:7257/books/page?RequestedPage=" + page + "&QuantityPerPage=" + quantity + "&InAscendingOrder=" + isAscending + "&OrderBy=Price");
+            
+        }
+
+        //List of all genres
+        public Uri GetListGenres { get; } = new Uri("https://localhost:7257/books/genres");
     }
 
     /**
