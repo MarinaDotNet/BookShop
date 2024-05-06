e1dd896eaccac0db756218fb077d53041736a295
Marina Sichova <msichova@outlook.com>
2024-05-06 13:39:37 +0800

    Created FilterModel.cs

---
+++ BookShop.WebApplication/Models/FilterModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop.WebApplication.Models
+{
+    public class FilterModel
+    {
+        public string? SelectedGenre { get; set; }
+
+        public string ? SearchTearm { get; set; }
+
+        public bool IsAscendingOrder { get; set; } = true;
+    }
+}
