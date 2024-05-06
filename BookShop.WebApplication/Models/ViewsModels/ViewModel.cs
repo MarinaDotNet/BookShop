5d8467fd6a2d7346a087e9f8d5aad90b182a1a66
Marina Sichova <msichova@outlook.com>
2024-05-06 13:40:38 +0800

    Created ViewModel.cs

---
+++ BookShop.WebApplication/Models/ViewsModels/ViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BookShop.WebApplication.Models.ViewsModels
+{
+    public class ViewModel
+    {
+        public string[] Genres { get; set; } = ["unspecified"];
+        public ProductViewModel ProductViewModel { get; set; } = new();
+        public PageViewModel PageViewModel { get; set; } = new();
+
+        public FilterModel FilterViewModel { get; set; } = new();
+    }
+}
