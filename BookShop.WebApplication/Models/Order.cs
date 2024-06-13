8de09cca2455c2f1be3b99f0fc2a6fe263a50b03
Marina Sichova <msichova@outlook.com>
2024-06-13 20:08:29 +0800

    Created Order.cs

---
+++ BookShop.WebApplication/Models/Order.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookShop.WebApplication.Models
+{
+    public class Order
+    {
+        public string ? OrderId { get; set; }
+
+        //public ApplicationUser User { get; set; } = new();
+        public string[] ? ProductsId { get; set; } = [];
+
+        public IEnumerable<Product> ? Products { get; set; } = [];
+
+        private decimal totalPrice = 0;
+        [Range(0, int.MaxValue)]
+        [Precision(18, 2)]
+        public decimal TotalPrice 
+        {
+            get => totalPrice;
+            set => totalPrice = Decimal.Parse(value.ToString());
+        }
+
+        DateTime orderDateTime;
+        public DateTime ? OrderDateTime 
+        { 
+            get => orderDateTime; 
+            set => orderDateTime = DateTime.Parse(value.ToString()!); 
+        }
+
+        public bool IsSubmitted { get; set; } = false;
+
+        public string ? Notes {  get; set; }
+    }
+}
